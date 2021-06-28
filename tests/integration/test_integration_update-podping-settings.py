e7635a0e188ca39503dbea4632951d60e0311942
brianoflondon <brian@podping.org>
2021-06-28 17:20:22 +0000

    fixed cross talk from config on other settings

--- tests/integration/test_integration_update-podping-settings.py
+++ tests/integration/test_integration_update-podping-settings.py
@@ -1,5 +1,5 @@
 import sys
-
+import asyncio
 import pytest
 
 from podping_hivewriter import config
@@ -10,14 +10,16 @@ from podping_hivewriter.run import update_podping_settings
 async def test_update_podping_settings():
     # See if we can fetch data from podping
     # Must not use Testnet when looking for config data
+
+    #Other tests are changing this setting:
+    config.Config.ignore_updates = False
+    
     test_account_check_period = sys.maxsize
     config.Config.podping_settings.control_account_check_period = (
         test_account_check_period
     )
     await update_podping_settings("podping")
-
+    await asyncio.sleep(2)
+    answer = config.Config.podping_settings.control_account_check_period
     # Compare properties specifically because we aren't overriding all default values
-    assert (
-        test_account_check_period
-        != config.Config.podping_settings.control_account_check_period
-    )
+    assert test_account_check_period != answer
