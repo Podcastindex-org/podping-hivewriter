bf7968bcfbcc83e0b020044318d47d01c0e973d6
Brian <brian@podping.org>
2021-09-17 16:07:53 +0300

    Allow for the use of continually updated Node list
    If a flag "use_fullnodeupdate" is true and found in the podping control account meta data, then use the list of nodes generated regularly by the hive account @fullnodesupdate.
    
    In my testing it is better to have this dynamic list of nodes than the ones I set on podping account.

--- src/podping_hivewriter/podping_settings.py
+++ src/podping_hivewriter/podping_settings.py
@@ -7,6 +7,8 @@ from podping_hivewriter.constants import PODPING_SETTINGS_KEY
 from podping_hivewriter.hive import get_hive
 from podping_hivewriter.models.podping_settings import PodpingSettings
 
+import asyncio
+
 
 async def get_settings_from_hive(
     nodes: Iterable[str], account_name: str
@@ -23,9 +25,23 @@ async def get_settings_from_hive(
         logging.error(f"posting_json_metadata for account {account_name} is empty")
 
 
+async def get_fullnodeupdate_settings() -> Iterable[str]:
+    """Return the most recent list of tested nodes from the @fullnodeupdate account"""
+    # See https://peakd.com/@fullnodeupdate
+    acc = Account("fullnodeupdate")
+    json_metadata = json.loads(acc["json_metadata"])
+    nodes = [item["node"] for item in json_metadata["report"] if item["hive"]]
+    return nodes
+
+
 async def get_podping_settings(
     nodes: Iterable[str], account_name: str
 ) -> PodpingSettings:
     """Return PodpingSettings object"""
-    settings_dict = await get_settings_from_hive(nodes, account_name)
+    tasks = [get_settings_from_hive(nodes, account_name), get_fullnodeupdate_settings()]
+    settings_dict, fullnodeupdate_nodes = await asyncio.gather(*tasks)
+    if settings_dict.get("use_fullnodeupdate"):
+        # Use the nodes from @fullnodeupdate if we find the flag "use_fullnodeupdate"
+        # in the reply from the control account.
+        settings_dict["main_nodes"] = fullnodeupdate_nodes
     return PodpingSettings.parse_obj(settings_dict)
