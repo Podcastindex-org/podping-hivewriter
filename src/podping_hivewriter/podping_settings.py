8dc4c060730aea578daf3305830d414f0f47350f
Brian of London <brian@podping.org>
2022-01-08 09:27:57 +0200

    remove ":=" to maintain python 3.7 compatability
    Signed-off-by: Brian of London <brian@podping.org>

--- src/podping_hivewriter/podping_settings.py
+++ src/podping_hivewriter/podping_settings.py
@@ -15,9 +15,11 @@ async def get_settings_from_hive(
     # Must use main chain for settings
     client: Client = get_client()
     account = client.account(account_name)
-    if raw_meta := account.raw_data.get("posting_json_metadata"):
+    raw_meta = account.raw_data.get("posting_json_metadata")
+    if raw_meta:
         metadata = json.loads(raw_meta)
-        if podping_settings := metadata.get(PODPING_SETTINGS_KEY):
+        podping_settings = metadata.get(PODPING_SETTINGS_KEY)
+        if podping_settings:
             return podping_settings
         else:
             logging.error(f"posting_json_metadata for account {account_name} is empty")
