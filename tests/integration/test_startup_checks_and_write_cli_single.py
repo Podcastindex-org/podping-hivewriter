e25b6571e719a0fea1b02c7ea4452f86343542ae
Brian of London <brian@podping.org>
2022-01-12 11:57:00 +0200

    new test script to check the startup.
    Signed-off-by: Brian of London <brian@podping.org>

--- tests/integration/test_startup_checks_and_write_cli_single.py
+++ tests/integration/test_startup_checks_and_write_cli_single.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-from timeit import default_timer as timer
 import uuid
 from platform import python_version as pv
 
@@ -19,13 +18,11 @@ from podping_hivewriter.podping_settings_manager import PodpingSettingsManager
 
 
 @pytest.mark.asyncio
-@pytest.mark.timeout(480)
+@pytest.mark.timeout(180)
 @pytest.mark.slow
-async def test_write_cli_single_simulcast():
-    """This test forces 6 separate posts to ensure we retry after exceeding the
-    limit of posts per block (5)"""
+async def test_startup_checks_and_write_cli_single():
     runner = CliRunner()
-    start = timer()
+
     settings_manager = PodpingSettingsManager(ignore_updates=True)
 
     client = Client()
@@ -33,15 +30,13 @@ async def test_write_cli_single_simulcast():
     session_uuid = uuid.uuid4()
     session_uuid_str = str(session_uuid)
 
+    test_name = "cli_single"
+    iri = f"https://example.com?t={test_name}&v={pv()}&s={session_uuid_str}"
+
     default_hive_operation_id = HiveOperationId(
         LIVETEST_OPERATION_ID, Medium.podcast, Reason.update
     )
     default_hive_operation_id_str = str(default_hive_operation_id)
-
-    async def _run_cli_once(_app, _args):
-        print(f"Timer: {timer()-start}")
-        result = runner.invoke(_app, _args)
-        return result
 
     async def get_iri_from_blockchain(start_block: int):
         event_listener = EventListener(client, "head", start_block=start_block)
@@ -51,55 +46,27 @@ async def test_write_cli_single_simulcast():
         ):
             data = json.loads(post["op"][1]["json"])
             if "iris" in data and len(data["iris"]) == 1:
-                iri = data["iris"][0]
-                # Only look for IRIs from current session
-                if iri.endswith(session_uuid_str):
-                    yield iri
+                yield data["iris"][0]
 
-    # Ensure hive env vars are set from .env.test file or this will fail
-
-    python_version = pv()
-    tasks = []
-    test_iris = {
-        f"https://example.com?t=cli_simulcast_{n}"
-        f"&v={python_version}&s={session_uuid_str}"
-        for n in range(6)
-    }
-    for iri in test_iris:
-        args = [
-            "--livetest",
-            "--no-sanity-check",
-            "--ignore-config-updates",
-            "--debug",
-            "write",
-            iri,
-        ]
-        tasks.append(_run_cli_once(app, args))
+    args = ["--livetest", "write", iri]
 
     current_block = client.get_dynamic_global_properties()["head_block_number"]
 
-    results = await asyncio.gather(*tasks)
+    # Ensure hive env vars are set from .env.test file or this will fail
+    result = runner.invoke(app, args)
 
-    all_ok = all(r.exit_code == 0 for r in results)
-    assert all_ok
+    assert result.exit_code == 0
 
     op_period = settings_manager._settings.hive_operation_period
 
     # Sleep to catch up because beem isn't async and blocks
     await asyncio.sleep(op_period * 25)
 
-    answer_iris = set()
-    async for stream_iri in get_iri_from_blockchain(current_block - 5):
-        answer_iris.add(stream_iri)
+    iri_found = False
 
-        # If we're done, end early
-        if len(answer_iris) == len(test_iris):
+    async for stream_iri in get_iri_from_blockchain(current_block - 5):
+        if stream_iri == iri:
+            iri_found = True
             break
 
-    assert answer_iris == test_iris
-
-
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    coro = test_write_cli_single_simulcast()
-    loop.run_until_complete(coro)
+    assert iri_found
