34a7b6a1f08b76fda2e568bf6dc0aa15b68cb194
Alecks Gates <alecks@podping.org>
2022-01-16 20:49:13 -0600

    Fix capnpy enum import for str->enum mapping under pypy

--- src/podping_hivewriter/models/medium.py
+++ src/podping_hivewriter/models/medium.py
@@ -11,8 +11,11 @@ Medium = medium_module.Medium
 
 mediums = frozenset(Medium.__members__)
 
+# capnpy has a different "constructor" for pyx vs pure python
+get_medium_by_num = Medium._new_hack if hasattr(Medium, "_new_hack") else Medium._new
+
 str_medium_map = {
-    enumerant.name.decode("UTF-8"): Medium._new_hack(enumerant.codeOrder)
+    enumerant.name.decode("UTF-8"): get_medium_by_num(enumerant.codeOrder)
     for enumerant in capnpy.get_reflection_data(medium_module)
     .get_node(Medium)
     .get_enum_enumerants()
--- src/podping_hivewriter/models/reason.py
+++ src/podping_hivewriter/models/reason.py
@@ -4,15 +4,18 @@ from capnpy.annotate import Options
 reason_module = capnpy.load_schema(
     "podping_hivewriter.schema.reason",
     # Make sure properties are imported as specified (camelCase)
-    options=Options(convert_case=False),
+    options=Options(convert_case=False, include_reflection_data=True),
 )
 
 Reason = reason_module.Reason
 
 reasons = frozenset(Reason.__members__)
 
+# capnpy has a different "constructor" for pyx vs pure python
+get_reason_by_num = Reason._new_hack if hasattr(Reason, "_new_hack") else Reason._new
+
 str_reason_map = {
-    enumerant.name.decode("UTF-8"): Reason._new_hack(enumerant.codeOrder)
+    enumerant.name.decode("UTF-8"): get_reason_by_num(enumerant.codeOrder)
     for enumerant in capnpy.get_reflection_data(reason_module)
     .get_node(Reason)
     .get_enum_enumerants()
