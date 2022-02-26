e793e23fba6a911d71b09d660c8c72af3b0f4f5f
Alecks Gates <alecks@podping.org>
2022-02-26 16:44:38 -0600

    Speedup tests by switching to lighthive stream to block_api
    This allows the tests to complete in a fraction of the time.

--- tests/integration/test_startup_checks_and_write_cli_single.py
+++ tests/integration/test_startup_checks_and_write_cli_single.py
@@ -1,20 +1,18 @@
-import asyncio
 import json
 import uuid
 from platform import python_version as pv
 
 import pytest
 from lighthive.client import Client
-from lighthive.helpers.event_listener import EventListener
 from typer.testing import CliRunner
 
-from podping_hivewriter.async_wrapper import sync_to_async
 from podping_hivewriter.cli.podping import app
 from podping_hivewriter.constants import (
     LIVETEST_OPERATION_ID,
     STARTUP_FAILED_INVALID_ACCOUNT,
     STARTUP_FAILED_INVALID_POSTING_KEY_EXIT_CODE,
 )
+from podping_hivewriter.hive import listen_for_custom_json_operations
 from podping_hivewriter.models.hive_operation_id import HiveOperationId
 from podping_hivewriter.models.medium import Medium
 from podping_hivewriter.models.reason import Reason
@@ -43,14 +41,11 @@ async def test_startup_checks_and_write_cli_single():
     default_hive_operation_id_str = str(default_hive_operation_id)
 
     async def get_iri_from_blockchain(start_block: int):
-        event_listener = EventListener(client, "head", start_block=start_block)
-        _on = sync_to_async(event_listener.on, thread_sensitive=False)
-        async for post in _on(
-            "custom_json", filter_by={"id": default_hive_operation_id_str}
-        ):
-            data = json.loads(post["op"][1]["json"])
-            if "iris" in data and len(data["iris"]) == 1:
-                yield data["iris"][0]
+        async for post in listen_for_custom_json_operations(client, start_block):
+            if post["op"][1]["id"] == default_hive_operation_id_str:
+                data = json.loads(post["op"][1]["json"])
+                if "iris" in data and len(data["iris"]) == 1:
+                    yield data["iris"][0]
 
     args = ["--livetest", "write", iri]
 
@@ -61,12 +56,9 @@ async def test_startup_checks_and_write_cli_single():
 
     assert result.exit_code == 0
 
-    # Sleep to catch up because beem isn't async and blocks
-    await asyncio.sleep(3 * 25)
-
     iri_found = False
 
-    async for stream_iri in get_iri_from_blockchain(current_block - 5):
+    async for stream_iri in get_iri_from_blockchain(current_block):
         if stream_iri == iri:
             iri_found = True
             break
