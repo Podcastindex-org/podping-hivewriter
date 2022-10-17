9714b60b859858930c303a2bd3f32752715a1351
Brian of London <brian@podping.org>
2022-10-17 19:18:58 +0300

    Updated mirrornet test to match new ZMQ system
    Signed-off-by: Brian of London <brian@podping.org>

--- tests/mirrornet/send_to_mirrornet_write_zmq_multiple.py
+++ tests/mirrornet/send_to_mirrornet_write_zmq_multiple.py
@@ -6,21 +6,31 @@ import logging
 import os
 import random
 import uuid
+from ipaddress import IPv4Address
 from platform import python_version as pv
 from random import randint
 
 import pytest
 import zmq
 import zmq.asyncio
+from plexo.ganglion.tcp_pair import GanglionZmqTcpPair
+from plexo.plexus import Plexus
 
 from podping_hivewriter.constants import LIVETEST_OPERATION_ID
 from podping_hivewriter.hive import get_client, listen_for_custom_json_operations
 from podping_hivewriter.models.hive_operation_id import HiveOperationId
 from podping_hivewriter.models.medium import mediums, str_medium_map
 from podping_hivewriter.models.reason import reasons, str_reason_map
+from podping_hivewriter.neuron import (
+    podping_hive_transaction_neuron,
+    podping_write_neuron,
+)
 from podping_hivewriter.podping_hivewriter import PodpingHivewriter
 from podping_hivewriter.podping_settings_manager import PodpingSettingsManager
+from podping_hivewriter.schema.podping_hive_transaction import PodpingHiveTransaction
+from podping_hivewriter.schema.podping_write import PodpingWrite
 
+LOOP_COUNT = 100_000
 
 @pytest.mark.asyncio
 @pytest.mark.timeout(3600)
@@ -38,15 +48,16 @@ async def test_write_zmq_multiple(event_loop):
     session_uuid = uuid.uuid4()
     session_uuid_str = str(session_uuid)
 
-    def get_test_iris():
-        num_iris = 100_000
-        test_name = "zmq_multiple"
+    def get_test_iris(num_iris: int = 1_000):
+        i = 0
+        test_name = "zmq_multiple_flood_test"
         python_version = pv()
-        test_iris = {
-            f"https://example.com?t={test_name}&i={i}&v={python_version}&s={session_uuid_str}"
-            for i in range(num_iris)
-        }
-        return test_iris
+        while i < num_iris:
+            yield (
+                f"https://example.com?t={test_name}"
+                f"&i={i}&v={python_version}&s={session_uuid_str}"
+            )
+            i += 1
 
     medium = str_medium_map[random.sample(sorted(mediums), 1)[0]]
     reason = str_reason_map[random.sample(sorted(reasons), 1)[0]]
@@ -54,48 +65,54 @@ async def test_write_zmq_multiple(event_loop):
     default_hive_operation_id = HiveOperationId(LIVETEST_OPERATION_ID, medium, reason)
     default_hive_operation_id_str = str(default_hive_operation_id)
 
-    async def get_iri_from_blockchain(start_block: int):
-        async for post in listen_for_custom_json_operations(client, start_block):
-            if post["op"][1]["id"] == default_hive_operation_id_str:
-                data = json.loads(post["op"][1]["json"])
-                if "iris" in data:
-                    for iri in data["iris"]:
-                        # Only look for IRIs from current session
-                        if iri.endswith(session_uuid_str):
-                            yield iri
+    tx_queue: asyncio.Queue[PodpingHiveTransaction] = asyncio.Queue()
+
+    async def _podping_hive_transaction_reaction(
+        transaction: PodpingHiveTransaction, _, _2
+    ):
+        await tx_queue.put(transaction)
 
     host = "127.0.0.1"
-    port = 9978 + randint(1,10000)
+    port = 9978 + randint(1, 10000)
     podping_hivewriter = PodpingHivewriter(
         os.environ["PODPING_HIVE_ACCOUNT"],
         [os.environ["PODPING_HIVE_POSTING_KEY"]],
         settings_manager,
-        medium=medium,
-        reason=reason,
+        # medium=medium,
+        # reason=reason,
         listen_ip=host,
         listen_port=port,
-        resource_test=True,
+        resource_test=False,
         operation_id=LIVETEST_OPERATION_ID,
     )
     await podping_hivewriter.wait_startup()
-    context = zmq.asyncio.Context()
-    socket = context.socket(zmq.REQ, io_loop=event_loop)
-    socket.connect(f"tcp://{host}:{port}")
 
-    op_period = settings_manager._settings.hive_operation_period
+    tcp_pair_ganglion = GanglionZmqTcpPair(
+        peer=(IPv4Address(host), port),
+        relevant_neurons=(
+            podping_hive_transaction_neuron,
+            podping_write_neuron,
+        ),
+    )
+    plexus = Plexus(ganglia=(tcp_pair_ganglion,))
+    # TODO: confirm we receive a valid transaction after success
+    await plexus.adapt(
+        podping_hive_transaction_neuron, reactants=(_podping_hive_transaction_reaction,)
+    )
+    await plexus.adapt(podping_write_neuron)
 
-    test_iris = get_test_iris()
-    for iri in test_iris:
-        await socket.send_string(iri)
-        response = await socket.recv_string()
-        await asyncio.sleep(0.01 * randint(0, 30))
-        assert response == "OK"
+    for iri in get_test_iris(LOOP_COUNT):
+        medium = str_medium_map[random.sample(sorted(mediums), 1)[0]]
+        reason = str_reason_map[random.sample(sorted(reasons), 1)[0]]
+        podping_write = PodpingWrite(medium=medium, reason=reason, iri=iri)
+        await asyncio.sleep(0.01 * randint(30, 100))
+        await plexus.transmit(podping_write)
 
     # Sleep until all items in the queue are done processing
     num_iris_processing = await podping_hivewriter.num_operations_in_queue()
     while num_iris_processing > 0:
-        await asyncio.sleep(op_period)
+        await asyncio.sleep(0.01 * randint(0, 30))
         num_iris_processing = await podping_hivewriter.num_operations_in_queue()
-        logging.info(num_iris_processing)
 
     podping_hivewriter.close()
+    assert True
