bb3a67bdc749c5e2b3e4653352f06186b3155af3
Alecks Gates <alecks@podping.org>
2022-12-09 18:50:32 -0600

    Fix method of cancelling tasks on close

--- src/podping_hivewriter/async_context.py
+++ src/podping_hivewriter/async_context.py
@@ -16,22 +16,28 @@ class AsyncContext:
         self.close()
 
     def close(self):
-        for task in self._tasks:
-            task.cancel()
-        wait_coro = asyncio.wait(
-            self._tasks, timeout=3, return_when=asyncio.ALL_COMPLETED
-        )
         try:
-            loop = asyncio.get_running_loop()
-
-            asyncio.ensure_future(
-                wait_coro,
-                loop=loop,
-            )
+            for task in self._tasks:
+                task.cancel()
         except RuntimeError:
-            asyncio.run(wait_coro)
-        finally:
-            self._tasks = []
+            pass
+
+        if self._tasks:
+            wait_coro = asyncio.wait(
+                self._tasks, timeout=3, return_when=asyncio.ALL_COMPLETED
+            )
+            try:
+                loop = asyncio.get_running_loop()
+
+                future = asyncio.run_coroutine_threadsafe(wait_coro, loop)
+                # This is broken, pending https://bugs.python.org/issue42130
+                future.result(3)
+            except RuntimeError:
+                asyncio.run(wait_coro)
+            except TimeoutError:
+                pass
+            finally:
+                self._tasks = []
 
     def _add_task(self, task):
         self._tasks.append(task)
